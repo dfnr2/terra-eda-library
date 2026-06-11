5c222bdba7047a0955d52731b58f525e3766781e
Dave <dfnum2@gmail.com>
2026-06-11 17:00:45 -0500

    consolidate inductors schema
    - add db/schema/types/inductors.sql (inductance, tolerance, current_rating, saturation_current, dc_resistance, self_res_freq)
    - register inductors (tier 2) in table_map.json
    - gen_schema generates inductors_0_schema.sql
    - remigrate_native rewrites inductors_1_migrated.sql (3 rows preserved)

---
+++ db/schema/types/inductors.sql
@@ -0,0 +1,8 @@
+-- db/schema/types/inductors.sql
+-- Canonical parametric tail for inductors (inductors).
+    inductance TEXT,
+    tolerance TEXT,
+    current_rating TEXT,
+    saturation_current TEXT,
+    dc_resistance TEXT,
+    self_res_freq TEXT
--- db/tables/inductors/inductors_1_migrated.sql
+++ db/tables/inductors/inductors_1_migrated.sql
@@ -7,61 +7,11 @@
 --
 
 -- Create symbols table
-CREATE TABLE IF NOT EXISTS inductors (
-  "unique_id" TEXT PRIMARY KEY,
-  "part_locator" TEXT,
-  "allow_substitution" TEXT,
-  "bom_comment" TEXT,
-  "class" TEXT,
-  "component_type" TEXT,
-  "component_value" TEXT,
-  "current_rating" TEXT,
-  "datasheet" TEXT,
-  "description" TEXT,
-  "kicad_footprint" TEXT,
-  "manufacturer" TEXT,
-  "manufacturer_link" TEXT,
-  "mpn" TEXT,
-  "composition" TEXT,
-  "number_of_pins" TEXT,
-  "package" TEXT,
-  "reference" TEXT,
-  "rohs" TEXT,
-  "rohs_document_link" TEXT,
-  "sim_device" TEXT,
-  "sim_library" TEXT,
-  "sim_pins" TEXT,
-  "sim_type" TEXT,
-  "standards_version" TEXT,
-  "kicad_symbol" TEXT,
-  "temp_operating" TEXT,
-  "temp_soldering" TEXT,
-  "temp_storage" TEXT,
-  "temp_coeff" TEXT,
-  "tolerance" TEXT,
-  "tracking" TEXT,
-  "value" TEXT,
-  "altium_footprint" TEXT,
-  "altium_symbol" TEXT,
-  "variant" TEXT,
-  "source" TEXT,
-  "dump_priority" INTEGER,
-    "tier" INTEGER DEFAULT 5,
-    "tags" TEXT DEFAULT '',
-    "lifecycle_status" TEXT DEFAULT 'Active',
-    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "created_by" TEXT,
-    "sim_model_type" TEXT,
-    "sim_model_file" TEXT,
-    "sim_params" TEXT
-);
 
--- Insert symbols
 BEGIN TRANSACTION;
 
-INSERT INTO inductors ("unique_id", "part_locator", "allow_substitution", "bom_comment", "class", "component_type", "component_value", "current_rating", "datasheet", "description", "kicad_footprint", "manufacturer", "manufacturer_link", "mpn", "composition", "number_of_pins", "package", "reference", "rohs", "rohs_document_link", "sim_device", "sim_library", "sim_pins", "sim_type", "standards_version", "kicad_symbol", "temp_operating", "temp_soldering", "temp_storage", "temp_coeff", "tolerance", "tracking", "value", "altium_footprint", "altium_symbol", "variant", "source", "dump_priority") VALUES ('Bourns-SRR6603-100ML q q', 'IND Bourns 10uH 1.1A rms SRR6603-100ML', 'No', NULL, NULL, 'Inductor', '10 uH', '1.1A rms, 40C rise, 1.5ADC, 10% drop', 'https://www.bourns.com/pdfs/SRR6603.pdf', 'Power inductor, 10 mH, 75 mOhm, 1A rms,  20% SMT 6.8x4.4mm', 'terra_sym:Coilcraft MSS7341 Shileded power inductor', 'Bourns', 'https://www.bourns.com/resources/rohs/magnetics/power-inductors-smd-shielded', 'SRR6603-100ML q q', NULL, '2', 'SMT', 'L', 'Yes', 'https://www.bourns.com/docs/rohs-cofc/cofc_srr.pdf?sfvrsn=7557d913_18', 'L', NULL, '1=+ 2=-', NULL, '1.1', 'terra_sym:IND Bourns 10uH 1.1A rms SRR6603-100ML', '-40C/+125C', 'Max: 260 °C for 5 sec, Reflow: 230 °C, 50 sec.', '-40C/+125C', NULL, '30%', 'No', 'IND Bourns 10uH 1.1A rms SRR6603-100ML', NULL, NULL, NULL, 'terra_sym', 100);
-INSERT INTO inductors ("unique_id", "part_locator", "allow_substitution", "bom_comment", "class", "component_type", "component_value", "current_rating", "datasheet", "description", "kicad_footprint", "manufacturer", "manufacturer_link", "mpn", "composition", "number_of_pins", "package", "reference", "rohs", "rohs_document_link", "sim_device", "sim_library", "sim_pins", "sim_type", "standards_version", "kicad_symbol", "temp_operating", "temp_soldering", "temp_storage", "temp_coeff", "tolerance", "tracking", "value", "altium_footprint", "altium_symbol", "variant", "source", "dump_priority") VALUES ('Bourns-SRR1210-680M', 'IND Bourns 68uH 3A rms  SRR1210-680M ', 'No', NULL, 'shielded', 'Inductor', '68 uH', '3A rms, 40C rise, 10% drop', 'https://www.bourns.com/docs/Product-Datasheets/SRR1210.pdf', 'Power inductor, 68 mH, 102 mOhm, 3A rms,  20% SMT 12mm x 12mm', 'terra_sym:Bourns SRR1210', 'Bourns', 'https://www.bourns.com/resources/rohs/magnetics/power-inductors-smd-shielded', 'SRR1210-680M', NULL, '2', 'SMT', 'L', 'Yes', 'https://www.bourns.com/docs/rohs-cofc/cofc_srr.pdf?sfvrsn=7557d913_18', 'L', NULL, '1=+ 2=-', NULL, '1.1', 'terra_sym:IND Bourns 68uH 3A rms  SRR1210-680M ', '-40C/+125C', 'Max: 260 °C for 5 sec, Reflow: 230 °C, 50 sec.', '-40C/+125C', NULL, '20%', 'No', 'IND Bourns 68uH 3A rms  SRR1210-680M', NULL, NULL, NULL, 'terra_sym', 100);
-INSERT INTO inductors ("unique_id", "part_locator", "allow_substitution", "bom_comment", "class", "component_type", "component_value", "current_rating", "datasheet", "description", "kicad_footprint", "manufacturer", "manufacturer_link", "mpn", "composition", "number_of_pins", "package", "reference", "rohs", "rohs_document_link", "sim_device", "sim_library", "sim_pins", "sim_type", "standards_version", "kicad_symbol", "temp_operating", "temp_soldering", "temp_storage", "temp_coeff", "tolerance", "tracking", "value", "altium_footprint", "altium_symbol", "variant", "source", "dump_priority") VALUES ('CoilCraft-MSS7341-103ML', 'IND CoilCraft 10uH 2.8A MSS7341-103ML', 'No', NULL, NULL, 'Inductor', '10 uH', '2.8A rms, 20C rise, 1.64ADC, 10% ind. drop', 'https://www.coilcraft.com/getmedia/7b464459-a4d6-47b0-83ca-9d96d4410863/MSS7341.pdf', 'Power inductor, 10 mH, 38 mOhm, 2.8A, 20% SMT 7.1x7.1 mm', 'terra_sym:Coilcraft MSS7341 Shileded power inductor', 'CoilCraft', 'https://www.coilcraft.com/en-us/products/power/shielded-inductors/ferrite-drum/mss-mos/mss7341/mss7341-103/', 'MSS7341-103ML', NULL, '2', 'SMT', 'L', 'Yes', 'https://4donline.ihs.com/images/VipMasterIC/IC/COLC/COLC-E-A0007342590/COLC-E-A0007342584-1.pdf?hkey=6D0214268300F1406B835FE51CB13195', 'L', NULL, '1=+ 2=-', NULL, '1.1', 'terra_sym:IND CoilCraft 10uH 2.8A MSS7341-103ML', '-40C/+85C', 'reflow 40 sec at max 260C', '-40C/+125C', NULL, '30%', 'No', 'IND CoilCraft 10uH 2.8A MSS7341-103ML', NULL, NULL, NULL, 'terra_sym', 100);
+INSERT INTO inductors ("unique_id", "part_locator", "mpn", "manufacturer", "package", "value", "description", "datasheet", "manufacturer_link", "kicad_symbol", "kicad_footprint", "rohs", "rohs_document_link", "allow_substitution", "tracking", "standards_version", "source", "dump_priority", "sim_device", "sim_pins", "pin_count", "temp_operating_min", "temp_operating_max", "temp_storage_min", "temp_storage_max", "tolerance", "current_rating") VALUES ('Bourns-SRR6603-100ML q q', 'IND Bourns 10uH 1.1A rms SRR6603-100ML', 'SRR6603-100ML q q', 'Bourns', 'SMT', 'IND Bourns 10uH 1.1A rms SRR6603-100ML', 'Power inductor, 10 mH, 75 mOhm, 1A rms,  20% SMT 6.8x4.4mm', 'https://www.bourns.com/pdfs/SRR6603.pdf', 'https://www.bourns.com/resources/rohs/magnetics/power-inductors-smd-shielded', 'terra_sym:IND Bourns 10uH 1.1A rms SRR6603-100ML', 'terra_sym:Coilcraft MSS7341 Shileded power inductor', 'Yes', 'https://www.bourns.com/docs/rohs-cofc/cofc_srr.pdf?sfvrsn=7557d913_18', 'No', 'No', '1.1', 'terra_sym', '100', 'L', '1=+ 2=-', '2', -40, 125, -40, 125, '30%', '1.1A rms, 40C rise, 1.5ADC, 10% drop');
+INSERT INTO inductors ("unique_id", "part_locator", "mpn", "manufacturer", "package", "value", "description", "datasheet", "manufacturer_link", "kicad_symbol", "kicad_footprint", "rohs", "rohs_document_link", "allow_substitution", "tracking", "standards_version", "source", "dump_priority", "sim_device", "sim_pins", "pin_count", "temp_operating_min", "temp_operating_max", "temp_storage_min", "temp_storage_max", "tolerance", "current_rating") VALUES ('Bourns-SRR1210-680M', 'IND Bourns 68uH 3A rms  SRR1210-680M ', 'SRR1210-680M', 'Bourns', 'SMT', 'IND Bourns 68uH 3A rms  SRR1210-680M', 'Power inductor, 68 mH, 102 mOhm, 3A rms,  20% SMT 12mm x 12mm', 'https://www.bourns.com/docs/Product-Datasheets/SRR1210.pdf', 'https://www.bourns.com/resources/rohs/magnetics/power-inductors-smd-shielded', 'terra_sym:IND Bourns 68uH 3A rms  SRR1210-680M ', 'terra_sym:Bourns SRR1210', 'Yes', 'https://www.bourns.com/docs/rohs-cofc/cofc_srr.pdf?sfvrsn=7557d913_18', 'No', 'No', '1.1', 'terra_sym', '100', 'L', '1=+ 2=-', '2', -40, 125, -40, 125, '20%', '3A rms, 40C rise, 10% drop');
+INSERT INTO inductors ("unique_id", "part_locator", "mpn", "manufacturer", "package", "value", "description", "datasheet", "manufacturer_link", "kicad_symbol", "kicad_footprint", "rohs", "rohs_document_link", "allow_substitution", "tracking", "standards_version", "source", "dump_priority", "sim_device", "sim_pins", "pin_count", "temp_operating_min", "temp_operating_max", "temp_storage_min", "temp_storage_max", "tolerance", "current_rating") VALUES ('CoilCraft-MSS7341-103ML', 'IND CoilCraft 10uH 2.8A MSS7341-103ML', 'MSS7341-103ML', 'CoilCraft', 'SMT', 'IND CoilCraft 10uH 2.8A MSS7341-103ML', 'Power inductor, 10 mH, 38 mOhm, 2.8A, 20% SMT 7.1x7.1 mm', 'https://www.coilcraft.com/getmedia/7b464459-a4d6-47b0-83ca-9d96d4410863/MSS7341.pdf', 'https://www.coilcraft.com/en-us/products/power/shielded-inductors/ferrite-drum/mss-mos/mss7341/mss7341-103/', 'terra_sym:IND CoilCraft 10uH 2.8A MSS7341-103ML', 'terra_sym:Coilcraft MSS7341 Shileded power inductor', 'Yes', 'https://4donline.ihs.com/images/VipMasterIC/IC/COLC/COLC-E-A0007342590/COLC-E-A0007342584-1.pdf?hkey=6D0214268300F1406B835FE51CB13195', 'No', 'No', '1.1', 'terra_sym', '100', 'L', '1=+ 2=-', '2', -40, 85, -40, 125, '30%', '2.8A rms, 20C rise, 1.64ADC, 10% ind. drop');
 
 COMMIT;
